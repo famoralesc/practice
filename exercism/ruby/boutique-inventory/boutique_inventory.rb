b340b5108df8f7bc1354ae9c57d00b5a562415e1
Felipe Morales C <fmoralesch@outlook.com>
2024-05-29 11:37:57 -0400

    (exercism): adding boutique inventory solution

---
+++ exercism/ruby/boutique-inventory/boutique_inventory.rb
@@ -0,0 +1,36 @@
+class BoutiqueInventory
+  def initialize(items)
+    @items = items
+  end
+
+  def item_names
+    @items.map{ | item | 
+      item[:name]
+  }.sort
+  end
+
+  def cheap
+    @items.select{|item| item[:price] <30}
+  end
+
+  def out_of_stock
+    @items.select{|item| item[:quantity_by_size].size == 0}
+  end
+
+  def stock_for_item(name)
+    item = @items.find{|item| item[:name] == name}
+    if item == nil
+      return {}
+    end
+    item[:quantity_by_size]
+  end
+
+  def total_stock
+    @items.map{ | item | 
+      item[:quantity_by_size].map{| _, stock| stock}
+  }.flatten.sum
+  end
+
+  private
+  attr_reader :items
+end
