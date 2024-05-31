1e10bc3cf158e82da1774704c8131f5d30cc007b
Felipe Morales C <fmoralesch@outlook.com>
2024-05-31 10:42:41 -0400

    (exercism): adding solution for Moviegoer problem

---
+++ exercism/ruby/moviegoer/moviegoer.rb
@@ -0,0 +1,31 @@
+# This is a custom exception that you can use in your code
+class NotMovieClubMemberError < RuntimeError
+end
+
+class Moviegoer
+  REGULAR_PRICE = 15
+  DISCOUNTED_PRICE = 10
+  MIN_AGE_FOR_DISCOUNT = 60
+  MIN_AGE_FOR_SCARY_MOVIES = 18
+
+  def initialize(age, member: false)
+    @age = age
+    @member = member
+  end
+
+  def ticket_price
+    @age < MIN_AGE_FOR_DISCOUNT ? REGULAR_PRICE : DISCOUNTED_PRICE
+  end
+
+  def watch_scary_movie?
+    @age < MIN_AGE_FOR_SCARY_MOVIES ? false : true
+  end
+
+  # Popcorn is 🍿
+  def claim_free_popcorn!
+    unless @member
+      raise NotMovieClubMemberError.new("Not movie club member error")
+    end
+    "🍿"
+  end
+end
